dca7d7092ae10ac96917188939bc82300c3e7d00
Francella Aguilar <francellaaguilar@Francellas-MBP.attlocal.net>
2022-05-30 15:08:07 -0400

    npm start not working

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/TechX', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/TechX', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
